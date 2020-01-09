448af97532276c8aa0d7b25740f61eeb137329f9
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2020-01-10 00:15:12 +0600

    added option

--- src/loginapp/option.java
+++ src/loginapp/option.java
@@ -1,5 +1,16 @@
 package loginapp;
 
 public enum option {
-    Admin, Student
+    Admin, Student;
+
+    private option() {
+    }
+
+    public String value() {
+        return name();
+    }
+
+    public static option fromvalue(String v) {
+        return valueOf(v);
+    }
 }
