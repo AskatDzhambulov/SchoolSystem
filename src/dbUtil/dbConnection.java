8cd4be8982f7f3bd7dd5fc87063ed7fbefcf2091
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2020-01-10 22:55:10 +0600

    small fixes

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -1,11 +1,8 @@
 package dbUtil;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import static java.lang.Class.forName;
 
 public class dbConnection {
 
@@ -14,7 +11,7 @@ public class dbConnection {
     private static final String CONN = "jdbc:mysql://localhost/login";
     private static final String SQCONN = "jdbc:sqlite:school.sqlite";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+    public static Connection getConnection() throws SQLException {
 
         try {
             Class.forName("org.sqlite.JDBC");
