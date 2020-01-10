250fcff5c70e3a970e485322125ee14422c5a45f
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2020-01-11 00:26:52 +0600

    added admin and student login in Login Controller

--- src/loginapp/LoginController.java
+++ src/loginapp/LoginController.java
@@ -1,11 +1,18 @@
 package loginapp;
 
+import Admin.AdminController;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import student.StudentsController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -40,10 +47,40 @@ public class LoginController implements Initializable {
     }
 
     public void studentLogin() {
+        try {
+            Stage userStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root  =  (Pane) loader.load(getClass().getResource("/student/studentFXML.fxml").openStream());
 
+            StudentsController studentsController = (StudentsController)loader.getController();
+
+            Scene scene = new Scene(root);
+            userStage.setScene(scene);
+            userStage.setTitle("Student Dashboard");
+            userStage.setResizable(false);
+            userStage.show();
+
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void adminLogin() {
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminroot = (Pane)adminLoader.load(getClass().getResource("/Admin/Admin.fxml").openStream());
 
+            AdminController adminController = (AdminController)adminLoader.getController();
+
+            Scene scene = new Scene(adminroot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashboard");
+            adminStage.setResizable(false);
+            adminStage.show();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
