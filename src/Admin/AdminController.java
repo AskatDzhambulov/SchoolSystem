3af2b4384fdea70e46eb45513291abf1ec4a9f6e
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2020-01-11 02:00:18 +0600

    new post to db in admin controller

--- src/Admin/AdminController.java
+++ src/Admin/AdminController.java
@@ -1,12 +1,23 @@
 package Admin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import dbUtil.dbConnection;
 
-public class AdminController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
 
     @FXML
     private TextField id;
@@ -32,4 +43,28 @@ public class AdminController {
     private TableColumn<StudentData, String> emailcolumn;
     @FXML
     private TableColumn<StudentData, String> dobcolumn;
+
+    private dbConnection dc;
+    private ObservableList<StudentData> data;
+
+    private String sql = "SELECT * FROM students";
+
+    public void initialize(URL url, ResourceBundle rb) {
+        this.dc = new dbConnection();
+    }
+
+    @FXML
+    private void loadStudentData(ActionEvent event)throws SQLException {
+        try {
+            Connection conn = dbConnection.getConnection();
+            this.data = FXCollections.observableArrayList();
+
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                this.data.add(new StudentData(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5)));
+            }
+        }catch (SQLException e) {
+            System.err.println("Error" + e);
+        }
+    }
 }
